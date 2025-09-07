1626d78d4ad0ccff4ba0379717d8bf527795d873
jay <jay@jays-Mac-Studio.local>
2025-09-08 02:19:06 +0900

    feat: Add sensor

--- action_io_component/actuator_base.py
+++ action_io_component/actuator_base.py
@@ -49,21 +49,21 @@ class Actuator(Generic[ST]):
     # ---- 공통 I/O ----
 
     def _read(self, start_addr, cnt) -> List[int]:
-        rr = self.client.read_holding_registers(start_addr, cnt, unit=self.reg['device_id'])
+        rr = self.client.read_holding_registers(start_addr, cnt, device_id=self.reg['device_id'])
         regs = rr.registers if rr else [0] * cnt
         return regs
 
     def send(self, cmd: Command) -> int:
         payload = self._encode_command(cmd)
         # 상태 체크하는건 상태머신에서
-        res = self.client.write_registers(self.reg['cmd_start_addr'], payload, unit=self.reg["device_id"])
+        res = self.client.write_registers(self.reg['cmd_start_addr'], payload, device_id=self.reg["device_id"])
         # TODO: 명령 보내고 결과를 받아오는 것 구현
         print(res)
         return self.now_opid
     
     def read_state(self) -> Dict:
-        unit, sa, cnt = self.reg["device_id"], self.reg["state_start_addr"], self.reg["state_cnt"]
-        rr = self._read(sa,cnt,unit)
+        device_id, sa, cnt = self.reg["device_id"], self.reg["state_start_addr"], self.reg["state_cnt"]
+        rr = self._read(sa,cnt,device_id)
         st = self._decode(rr)
         return asdict(st)
 
