843fb9dacdc352d831d1c2b4f690f8df678f21cd
jay <jay@jays-Mac-Studio.local>
2025-09-10 03:16:13 +0900

    feat: Add rule runner

--- rule_engine/rules_runner.py
+++ rule_engine/rules_runner.py
@@ -1,15 +1,18 @@
 from sqlalchemy import create_engine, text
 from query import get_query
 import os
+from rule_decider import load_rules, decide_rules
 
 db_url = os.environ.get("DATABASE_URL")
 
 engine = create_engine(db_url)
 
-
+# 센서값 받아옴
 with engine.connect() as conn:
     res = conn.execute(text(get_query()))
     row = res.first()
     res = dict(row._mapping) if row else None
 
-print(res)
+
+rules  = load_rules("rules_conf")
+print(decide_rules(res, rules))
